629f126db1be52325b69667673b5e0b37f1aec70
paillao <paillao@gmail.com>
2022-06-05 21:02:34 -0400

    React 17 Ecosystem Start Proyect From Scratch, Webpack Ready

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        options: { presets: ['@babel/env'] }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  output: {
+    path: path.resolve(__dirname, 'dist/'),
+    publicPath: '/dist/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port: 3000,
+    hot: 'only'
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
