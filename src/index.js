f6ba4327da07bc04afab89c72da7706688691e29
paillao <paillao@gmail.com>
2022-06-08 23:16:44 -0400

    App To do, Redux Persist Config Test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { persistStore } from 'redux-persist';
+import { persistGate } from 'redux-persist/lib/integration/react';
 import { Provider } from 'react-redux';
 import { configureStore } from './store';
 import App from './App.js';
 
+const store = configureStore();
+const persistor = persistStore(store);
+
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
